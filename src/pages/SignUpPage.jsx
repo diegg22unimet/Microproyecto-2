1398a7148063d5e0609d6fb2295e8c64a95fae40
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2024-03-09 12:44:16 -0400

    SignUpPage estructura lista

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,42 +1,54 @@
 import styles from "./SignUpPage.module.css"
+import videojuegos from '../videogames.json';
 
 function SignUpPage(){
     return (
         <div>
-            <div>
+            <div className={styles.title}>
                 <h1>Registro de usuario</h1>
             </div>
             <div>
                 <form action="">
-                    <div>
-                        <label htmlFor="">Nombre</label>
-                        <input type="text" />
-                        <label htmlFor="">Apellido</label>
+                    <div className={styles.row}>
+                        <label htmlFor="name">Nombre</label>
+                        <br />
+                        <input type="text" value="Primer nombre"/>
+                        <input type="text" value="Primer apellido"/>
+                    </div>
+                    <br />
+                    <div className={styles.row}>
+                        <label htmlFor="username">Nombre de usuario</label>
+                        <br />
                         <input type="text" />
                     </div>
                     <br />
-                    <div>
-                        <label htmlFor="">Nombre de usuario</label>
+                    <div className={styles.row}>
+                        <label htmlFor="email">Correo electrónico</label>
+                        <br />
+                        <input type="email" value="Email"/>
+                    </div>
+                    <br />
+                    <div className={styles.row}>
+                        <label htmlFor="password">Contraseña</label>
+                        <br />
                         <input type="text" />
                     </div>
                     <br />
-                    <div>
-                        <label htmlFor="">Correo electrónico</label>
-                        <input type="text" />
-                    </div>
-                    <br />
-                    <div>
-                        <label htmlFor="">Contraseña</label>
-                        <input type="text" />
-                    </div>
-                    <br />
-                    <div>
-                        <label htmlFor="">Videojuego favorito</label>
+                    <div className={styles.row}>
+                        <label htmlFor="videogame">Videojuego favorito</label>
                         <select name="" id="">
-                            <option value=""></option>
+                            {videojuegos.map((videojuego) => (
+                                <option key={videojuego.ID} value={videojuego.titulo}>
+                                {videojuego.titulo}
+                                </option>
+                            ))}
                         </select>
                     </div>
                 </form>
+                <br />
+                <div className={styles.signup_button}>
+                    <button type="button">Registrarse</button>
+                </div>
             </div>
         </div>
     )
