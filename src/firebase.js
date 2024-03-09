72aee9a14d41fea4d28cfba7c2a907be295f8faa
Users <juan.figuera@correo.unimet.edu.vegit config --global user.name Juan>
2024-03-08 21:05:27 -0400

    Firebase configurado

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYoSBRjYNWZw74GnD-C_3IKWMs5ubanxU",
+  authDomain: "microproyecto2-a0828.firebaseapp.com",
+  projectId: "microproyecto2-a0828",
+  storageBucket: "microproyecto2-a0828.appspot.com",
+  messagingSenderId: "675816795076",
+  appId: "1:675816795076:web:babe4a20032e2110841291"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+
+export const router = createBrowserRouter ([
+    {
+        path:'/home',
+        element: <HomePage/>,
+    },
+    {
+        path:'/login',
+        element: <LoginPage/>,
+    }
+
+
+]); 
