34c797ed9dff5093886e05bb07f20611d7b09fea
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2024-03-09 16:43:43 -0400

    Modificaciones CSS SignUpPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,24 @@
-export default function HomePage(){
-    return <h1>Hello world!</h1>
+import React from 'react';
+import styles from './HomePage.module.css';
+import clubsData from '../clubs.json';
+import ClubCard from './ClubCard';
+
+function HomePage(){
+    return (
+        <div>
+            <div className={styles.navbar}>
+                <nav>
+                    <a href="./home">Home</a>
+                    <a href="./login">Sign In</a>
+                    <a href="./signup">Sign Up</a>
+                </nav>
+            </div>
+            <div className={styles.clubsContainer}>
+                {clubsData.map((club) => (
+                    <ClubCard key={club.ID} club={club} />
+                ))}
+            </div>
+        </div>
+    )
 }
+export default HomePage
