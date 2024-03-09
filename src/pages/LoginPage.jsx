0f1e943ef9b79a7ef1739b26bdffd52f742bdb29
Users <juan.figuera@correo.unimet.edu.vegit config --global user.name Juan>
2024-03-09 14:29:04 -0400

    Login google

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,30 @@
+import{ signInWithPopup} from "firebase/auth";
+import {auth, googleProvider} from '../firebase'
+import { useUser } from "../context/user";
+import { loginWithCredencials } from "../controllers/auth";
+
 export default function LoginPage(){
-    return <h1>login</h1>
+    const user = useUser();
+
+    async function handleClick() {
+        const result = await signInWithPopup(auth,googleProvider);
+        console.log(result);
+    }
+
+    const handleLogin = async () => {
+        const user = await loginWithCredencials(email,password)
+
+
+    }
+
+
+    return (
+        <div>
+            <button onClick={handleLogin}>  Iniciar Sesion  </button>
+
+            <button onClick={handleClick}>  Iniciar Sesion con Google  </button>
+
+        </div>
+
+    )
 }
