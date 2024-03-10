28b231ef22446a7941eb16b3c62b1fe58be6622a
Users <juan.figuera@correo.unimet.edu.vegit config --global user.name Juan>
2024-03-10 12:34:54 -0400

    Cartas

--- src/pages/ClubsPage.jsx
+++ src/pages/ClubsPage.jsx
@@ -1,23 +1,29 @@
 import Card from "../Card";
-import  { useClubs } from "../hooks/api";
+import { useClubs } from "../hooks/api"
 
-
-
-
-
-export default function Cardpage(){
+export default function ClubsPage(){
     const clubs = useClubs();
 
+    return (
 
-    return(
-        
+        <div>
 
-        
-        clubs.data.map(({title,description,games}) => (
-            <Card key={title} title = {title} description = {description} games = {games}></Card>
+            {clubs.isLoading && <div>Se estan cargando los clubes </div>}
 
-        ))
-        
+            {clubs.isLoading ? (
+
+                <div> Cargando....</div>
+
+            ):(
+
+                clubs.data.map(({title,description,games}) =>(
+                    <Card key={title} name={title} description={description} games={games}/>
+
+
+                ))
+            )}
+
+
+        </div>
     )
-
 }
